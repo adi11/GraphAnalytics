49ed6b8919f06b9e73fe43c8a2be51b18e01f17f
Aditya <webmasteradi@gmail.com>
2015-06-23 22:23:56 +0530

    Initial Project

---
+++ README.md
@@ -0,0 +1,2 @@
+Motive of this project is to convert XML files from Stackoverflow data dump to Titan readable JSON format.
+I am using Titan wit Cassandra as backend.
---
+++ clean_posts_json.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+
+
+
+import re
+import os
+
+#Author : Aditya Choudhary 
+
+#DES = os.getcwd()	# gives dir. of teminal, from which script is run
+DES = os.path.dirname(os.path.abspath(__file__))	# gives dir. of script
+
+pattern1 = '"_'
+pattern2 = '_Id"'
+
+subst1 = '"'
+subst2 = '__id"'
+
+with open(DES+"/"+"Posts_1c.json",'w') as new_file:
+	with open(DES+"/"+"Posts_1.json") as old_file:
+		for line in old_file:
+			s= line
+			#s.replace(pattern1,subst1)
+			#line.replace(pattern2,subst2)
+			#print line
+			line = re.sub(pattern2,subst2,line)
+			line = re.sub(pattern1,subst1,line)
+			#print line
+			new_file.write(line)
+
+#close(old_file)
+
